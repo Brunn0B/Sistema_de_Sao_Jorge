01c295c69a04112eb6691bf7cc8e035b3c4e54df
Brunn0B <brito.brunnob@gmail.com>
2025-01-26 22:12:39 -0300

    Teste-Leitura-Camera-Digitalizaçao.

--- utils/ocr.js
+++ utils/ocr.js
@@ -1,13 +1,13 @@
 const Tesseract = require("tesseract.js");
 
-async function extractTextFromImage(imagePath) {
+async function extractTextFromImage(imageData) {
   try {
-    const result = await Tesseract.recognize(imagePath, "por"); // 'por' para português
-    return result.data.text;
-  } catch (error) {
-    console.error("Erro ao processar OCR:", error);
+    const { data } = await Tesseract.recognize(imageData, "por"); // "por" para português
+    return data.text;
+  } catch (err) {
+    console.error("Erro ao processar OCR:", err);
     return null;
   }
-  
 }
+
 module.exports = { extractTextFromImage };
